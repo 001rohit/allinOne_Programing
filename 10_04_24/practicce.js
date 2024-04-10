be0453f771ed6a0ef210fac01975311e455c2490
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-04-10 22:06:31 +0530

    logic based

---
+++ 10_04_24/practicce.js
@@ -0,0 +1,18 @@
+
+var b 
+console.log(b);
+
+let c;
+console.log(c);
+
+// const a;
+// console.log(a);
+
+
+// function example(){
+//     console.log(a);
+//     console.log(b);
+//     var b=1;
+//     let a=2; 
+// }
+// example()
---
+++ 10_04_24/practice.js
@@ -0,0 +1,22 @@
+// table
+// for(let i=1;i<=10;i++){
+//     for(let j=2;j<=2;j++){
+//         console.log(j*i);
+//     }
+// }
+
+// sum
+// let sum=0;
+// for(let i=0;i<=10;i++){
+//     sum+=i
+// }
+// console.log(sum);
+
+// factorial
+// let fact=1;
+// for(let i=1;i<=5;i++){
+//     fact*=i
+// }
+// console.log(fact);
+
+
